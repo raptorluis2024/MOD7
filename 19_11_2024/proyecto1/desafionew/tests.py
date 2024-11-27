36631b15002be319156418124ac648e1d752c308
Luis Peña H <raptor_luis@hotmail.com>
2024-11-27 19:26:31 -0300

    .

--- 19_11_2024/proyecto1/desafionew/tests.py
+++ 19_11_2024/proyecto1/desafionew/tests.py
@@ -10,18 +10,19 @@ from desafionew.services import crearProfesor
 
 #for d in lista:
 #    print(d.estudiante, d.seccion.profesor, d.seccion)
-"""
+
 lista = listarEstudiantes()
+print(lista)
 print(lista["titulo"])
 for e in lista["estudiantes"]:
     print(e)
-"""
+
 lista = listadoSecciones()
 for l in lista:
     print(l["estudiantes"])
     
 
 #print(crearCurso(111,"CURSO TEST1", 1))
-#for c in listarCursos():
+#for c in listsarCursos():
 #    print(c)
 #print(crearProfesor(2222,"bbbbb","bbbbbb",True,"LPH"))
