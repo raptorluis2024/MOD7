22ace762a5589ce6cd55fafcd7b70e83afcc08e4
Luis Peña H <raptor_luis@hotmail.com>
2024-11-27 21:52:14 -0300

    add cambios modelo

--- proyecto_inmueble/gestion_inmueble/admin.py
+++ proyecto_inmueble/gestion_inmueble/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from gestion_inmueble.models import Comuna, Region
+from gestion_inmueble.models import Comuna, Region, Inmueble
+from gestion_inmueble.models import Profile,Tipo_inmueble,Tipo_Usuario
+
 # Register your models here.
+@admin.register(Tipo_Usuario)
+class Tipo_UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('codigo_tipo_usuario','tipo_user')
+    list_display_links = ('codigo_tipo_usuario',)
+    
+@admin.register(Tipo_inmueble)
+class Tipo_inmuebleAdmin(admin.ModelAdmin):
+    list_display = ('codigo_tipo_inmueble','tipo_inmueble')
+    list_display_links = ('codigo_tipo_inmueble',)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('rut','direccion', 'telefono')
+    list_display_links = ('rut',)
+
+@admin.register(Inmueble)
+class InmuebleAdmin(admin.ModelAdmin):
+    list_display = ('id_inmueble','nombre_inmueble', 'descripcion')
+    list_display_links = ('id_inmueble',)
+
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ('id_region','nombre_region')
--- proyecto_inmueble/gestion_inmueble/models.py
+++ proyecto_inmueble/gestion_inmueble/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Region(models.Model):
@@ -6,7 +7,7 @@ class Region(models.Model):
     nombre_region = models.CharField(max_length=40, null=False, blank=False)
     
     def __str__(self):
-        return f"{self.id_region}"
+        return f"{self.id_region}-{self.nombre_region}"
     
 class Comuna(models.Model):
     id_comuna = models.IntegerField(primary_key=True)
@@ -17,7 +18,53 @@ class Comuna(models.Model):
         unique_together = ('id_comuna', 'region')
     
     def __str__(self):
-        return f"{self.id_comuna}"
+        return f"{self.id_comuna}-{self.nombre_comuna}"
+    
+class Tipo_Usuario(models.Model):
+    codigo_tipo_usuario = models.IntegerField(primary_key=True)
+    tipo_user = models.CharField(max_length=50, blank=False, null=False)
+    
+    def __str__(self):
+        return f"{self.codigo_tipo_usuario}-{self.tipo_user}"
+
+class Tipo_inmueble(models.Model):
+    codigo_tipo_inmueble = models.IntegerField(primary_key=True)
+    tipo_inmueble = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return f"{self.codigo_tipo_inmueble}-{self.tipo_inmueble}"
+   
+class Profile(models.Model):
+    rut = models.CharField(max_length=10, primary_key=True)
+    direccion = models.CharField(max_length=50, null=False, blank= False)
+    telefono = models.CharField(max_length=20)
+    correo = models.EmailField(null=False, blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_tipo_user = models.ForeignKey(Tipo_Usuario,on_delete=models.CASCADE,null=True,)
+    
+    def __str__(self):
+        return self.rut
+ 
+class Inmueble(models.Model):
+    id_inmueble = models.IntegerField(primary_key=True)
+    nombre_inmueble = models.TextField()
+    descripcion = models.CharField(max_length=200)
+    m2_construido = models.FloatField()
+    numero_banos = models.IntegerField(default =1, null=False, blank=False)
+    numero_hab = models.IntegerField(default =1)
+    direccion = models.CharField(max_length=200)
+    id_user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,)
+    id_tipo_inmueble = models.ForeignKey(Tipo_inmueble,on_delete=models.CASCADE,null=True,)
+    id_comuna = models.ForeignKey(Comuna,on_delete=models.CASCADE,null=True,)
+    
+    def __str__(self):
+        return f"{self.id_inmueble}-{self.nombre_inmueble}"
+
+
+    
+    
+
+    
     
 
     
