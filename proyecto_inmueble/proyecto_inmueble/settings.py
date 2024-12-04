d6f7d7735b772e48c4cfe65388a258c688850aa6
Luis Peña H <raptor_luis@hotmail.com>
2024-12-04 20:30:32 -0300

    fix setting

--- proyecto_inmueble/proyecto_inmueble/settings.py
+++ proyecto_inmueble/proyecto_inmueble/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
+
+STATICFILES_DIRS=( os.path.join(BASE_DIR,'static'),)
+#STATIC_ROOT='static'
