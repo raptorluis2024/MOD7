215f38e656b85f77ff4d1ec48bce975ab10c3cad
Luis Pena Herrera <raptor_luis@hotmail.com>
2024-12-05 13:42:24 -0300

    add quer raw correcta

--- proyecto_inmueble/gestion_inmueble/temp.py
+++ proyecto_inmueble/gestion_inmueble/temp.py
@@ -14,28 +14,28 @@ def getComunas(id_region):
     """
 
     query = Comuna.objects.raw(select)
-    print(query)
+    print(query, "total de filas ", len(query), type(query))
     for r in query:
         print(r, type(r))
 
 def getInmuebles(comuna):
-    consulta = f"""
-    SELECT gii.id_inmueble, gii.nombre_inmueble,
-    gic.nombre_comuna, gir.nombre_region 
+    consulta = f"""SELECT gii.id_inmueble, gii.nombre_inmueble,gic.nombre_comuna, gir.nombre_region
     FROM public.gestion_inmueble_inmueble gii 
     inner join public.gestion_inmueble_comuna gic 
     on gii.id_comuna_id = gic.id_comuna 
     inner join public.gestion_inmueble_region gir 
     on gic.region_id = gir.id_region 
-    where gic.nombre_comuna like '%{comuna}%'
+    where gic.nombre_comuna  like %s 
     """
+
+    params = ['%' + comuna + '%']
     
     print(consulta)
-    query = Inmueble.objects.raw(consulta)
-    print(query)
-    for i in query:
+    resultado = Inmueble.objects.raw(consulta,params)
+    
+    for i in resultado:
         print(i)
         
 getComunas(13)
-getInmuebles('MAI')
+getInmuebles('SAN')
     
