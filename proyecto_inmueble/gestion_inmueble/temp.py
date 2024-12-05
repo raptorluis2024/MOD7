9171ec72830bbf1e10977e0c8283bf1d0a077147
Luis Peña H <raptor_luis@hotmail.com>
2024-12-04 21:47:53 -0300

    add temp

--- proyecto_inmueble/gestion_inmueble/temp.py
+++ proyecto_inmueble/gestion_inmueble/temp.py
@@ -1,12 +1,41 @@
+import sys
+from pathlib import Path
 import os
 import django
+sys.path.append(Path(__file__).resolve().parent.parent.__str__())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","proyecto_inmueble.settings")
 django.setup()
-from .models import Region
+from gestion_inmueble.models import Comuna, Inmueble
 
-select = "select * FROM gestion_inmueble_comuna WHERE region_id = 1"
+def getComunas(id_region):
+    select = f"""
+    select * FROM gestion_inmueble_comuna 
+    WHERE region_id = {id_region}
+    """
 
-query = Region.objects.raw(select)
-for r in query:
-    print(r)
+    query = Comuna.objects.raw(select)
+    print(query)
+    for r in query:
+        print(r, type(r))
+
+def getInmuebles(comuna):
+    consulta = f"""
+    SELECT gii.id_inmueble, gii.nombre_inmueble,
+    gic.nombre_comuna, gir.nombre_region 
+    FROM public.gestion_inmueble_inmueble gii 
+    inner join public.gestion_inmueble_comuna gic 
+    on gii.id_comuna_id = gic.id_comuna 
+    inner join public.gestion_inmueble_region gir 
+    on gic.region_id = gir.id_region 
+    where gic.nombre_comuna like '%{comuna}%'
+    """
+    
+    print(consulta)
+    query = Inmueble.objects.raw(consulta)
+    print(query)
+    for i in query:
+        print(i)
+        
+getComunas(13)
+getInmuebles('MAI')
     
